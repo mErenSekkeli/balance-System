dbaf399a9ea37b043d98e521e5b902805c6fa3b2
Muhammed Eren Şekkeli <erensekkeli46@gmail.com>
2022-03-31 16:31:53 +0300

    project created

---
+++ src/Connector.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connector {
+    
+    private String admin_ad="sistemanalizi";
+    private String admin_pass="SISTEManalizi123";
+    private String db_name="sistemAnalizi";
+    private String host="207.154.220.30";
+    private int port=3306;
+    private Connection con=null;
+    private Statement state=null;
+    private PreparedStatement preState=null;
+    
+    public Connector(){
+        
+        String url="jdbc:mysql://"+host+":"+port+"/"+db_name+"?useUnicode=true&characterEncoding=utf8";
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Basarili");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Driver Bulunamadi...");
+        }
+        
+        try {
+            System.out.println("Baglanti Yapiliyor...");
+            con=DriverManager.getConnection(url,admin_ad,admin_pass);
+            System.out.println("Baglanti Basarili...");
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            System.out.println("Veri Tabanina Baglanti Basarisiz...");
+        }
+        
+    }
+    public static void main(String[] args) {
+        Connector db=new Connector();
+        
+    }
+    
+}

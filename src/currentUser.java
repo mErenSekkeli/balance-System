32329eb43c88f19d92136694bb35eeb77be08188
Selahattin Emre Kutlu <48654612+sel0nidas@users.noreply.github.com>
2022-05-21 11:36:51 +0300

    Add files via upload

--- src/currentUser.java
+++ src/currentUser.java
@@ -32,6 +32,24 @@ public class currentUser{
         }
     }
     
+    public static int getAccount_role(){
+        try{
+            int account_id = -1;
+            String query = "SELECT * FROM accounts where account_online = 1";
+            db.preState=db.con.prepareStatement(query);
+            ResultSet rs=db.preState.executeQuery();
+        
+            if(rs != null){
+                rs.next();    
+                account_id = rs.getInt("account_role");
+            }
+            return account_id;
+        }
+        catch (SQLException ex) {
+            return -1;
+        }
+    }
+    
     public static void main(String args[]) {
         System.out.println(currentUser.getAccount_id());
     }

e5502dda085ed72e9801ea3b98bbd49c2876e84b
Selahattin Emre Kutlu <48654612+sel0nidas@users.noreply.github.com>
2022-05-21 11:36:24 +0300

    Add files via upload

--- src/registerPage.java
+++ src/registerPage.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -14,11 +15,12 @@ import java.util.Date;
 import java.text.SimpleDateFormat;  
 
 public class registerPage extends javax.swing.JFrame {
+    
     private int val=0;
-    private String mes="Kayit Basarili! Yonlendiriliyorsunuz";
     public registerPage() {
-        initComponents();
-    }  
+        initComponents();    } 
+    
+    
     private static Connector db=new Connector();
     private String query = "INSERT INTO accounts(account_email, account_password_hash, account_role) VALUES (?, ?, ?)";
     
@@ -26,10 +28,10 @@ public class registerPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        button1 = new java.awt.Button();
         user_mail = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        user_role = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         user_pass = new javax.swing.JPasswordField();
@@ -38,17 +40,24 @@ public class registerPage extends javax.swing.JFrame {
         info_message = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         user_name = new javax.swing.JTextField();
+        user_role = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+
+        button1.setLabel("button1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Kullanici Kaydi");
+        setPreferredSize(new java.awt.Dimension(1280, 720));
+        setSize(new java.awt.Dimension(1280, 720));
 
-        jLabel1.setText("Account ");
+        jLabel1.setText("Email");
 
-        jLabel2.setText("Account Role");
+        jLabel2.setText("Kullanıcı Rolü");
 
-        jLabel3.setText("Sifre:");
+        jLabel3.setText("Şifre");
 
-        jLabel4.setText("Sifre Tekrar:");
+        jLabel4.setText("Şifre Tekrar");
 
         user_registerBTN.setText("Kaydol");
         user_registerBTN.addActionListener(new java.awt.event.ActionListener() {
@@ -59,75 +68,117 @@ public class registerPage extends javax.swing.JFrame {
 
         info_message.setText(" ");
 
-        jLabel5.setText("Username");
+        jLabel5.setText("Ad Soyad");
+
+        user_role.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Seçiniz", "admin", "satış işlemleri", "stok işlemleri", " " }));
+        user_role.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                user_roleActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Giriş Yap");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("KAYIT OL");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(216, 216, 216)
-                .addComponent(user_registerBTN)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(66, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(267, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 99, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(info_message, javax.swing.GroupLayout.PREFERRED_SIZE, 371, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 81, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(user_mail, javax.swing.GroupLayout.DEFAULT_SIZE, 170, Short.MAX_VALUE)
-                            .addComponent(user_role)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(info_message, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(472, 472, 472))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(user_registerBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(user_mail)
                             .addComponent(user_pass)
                             .addComponent(user_pass2)
-                            .addComponent(user_name))))
-                .addGap(63, 63, 63))
+                            .addComponent(user_name)
+                            .addComponent(user_role, 0, 367, Short.MAX_VALUE))
+                        .addGap(428, 428, 428))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(81, 81, 81)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(user_mail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
+                .addGap(24, 24, 24)
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
+                    .addComponent(user_mail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(jLabel1)))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(jLabel2))
                     .addComponent(user_role, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(jLabel3))
                     .addComponent(user_pass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(jLabel4))
                     .addComponent(user_pass2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(jLabel5))
                     .addComponent(user_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26)
-                .addComponent(user_registerBTN)
                 .addGap(18, 18, 18)
-                .addComponent(info_message)
-                .addContainerGap(55, Short.MAX_VALUE))
+                .addComponent(user_registerBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(info_message)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(156, 156, 156))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
+    
+    
+    
     private void user_registerBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_user_registerBTNActionPerformed
-        
+
+    
         try {
+           // test.account_id=15;
             
-            String account_role=user_role.getText();
+            int account_role=user_role.getSelectedIndex();
             String username=user_name.getText();
             String mail=user_mail.getText();
             String pass = user_pass.getText();
@@ -135,29 +186,46 @@ public class registerPage extends javax.swing.JFrame {
             
             
             
-            if(pass.length() < 8){
+            if(!emailValidation.isValid(mail)){
+                info_message.setBackground(new Color(37, 146, 67));
+                info_message.setText("Kullanıcı Kaydı Başarısız: Girilen email adresinin formatı düzgün değil");
+                
+            }
+            else if( account_role ==0  ){
                 
                 info_message.setBackground(new Color(37, 146, 67));
-                info_message.setText("Kullanici Kaydi basarisiz: Sifre uzunlugu en az 8 karakter olmalı");
+                info_message.setText("Kullanıcı Rolü Seçiniz!");
+                
+            }
+            else if(pass.length() < 8){
+                
+                info_message.setBackground(new Color(37, 146, 67));
+                info_message.setText("Kullanıcı Kaydı Başarısız: Şifre Uzunluğu En Az 8 Karakter Olmalı");
             }
             else if(pass.compareTo(pass2) != 0){
                 info_message.setBackground(new Color(37, 146, 67));
-                info_message.setText("Kullanici Kaydi basarisiz: Girdiginiz iki sifre uyusmuyor");
-            }   
+                info_message.setText("Kullanıcı Kaydı Başarısız: Girdiğiniz İki Şifre Uyuşmuyor");
+            } 
             else{ 
             
+                try {
+                pass = md5Hash.toHexString(md5Hash.getSHA(pass));
+                } 
+                catch (NoSuchAlgorithmException ex) {
+                    Logger.getLogger(registerPage.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 
             db.preState=db.con.prepareStatement(query);
             db.preState.setString(1, mail);
             db.preState.setString(2, pass);
-            db.preState.setString(3, account_role);
+            db.preState.setString(3, Integer.toString(account_role));
             int check = db.preState.executeUpdate();
             
             
             // kullanıcı kaydı başarılı ise kullanıcı girişi yapması için yönlendirme yapılır.
             
             
-            System.out.println("Kullanici kaydi basarili kullanici giris yapmasi icin yonlendiriliyor");
+            System.out.println("Kullanıcı Kaydı Başarılı: Kullanıcı Giriş Yapması İçin Yönlendiriliyor");
             
             // Kullanıcının accounts tablosundaki insert edildiğinde oluşan id'si employer tablosundaki ile eşlenip kullanıcı o tabloya da insert ediliyor
             query = "SELECT * FROM accounts where account_email = ? and account_password_hash = ?";
@@ -170,10 +238,11 @@ public class registerPage extends javax.swing.JFrame {
                 rs.next();
                 
                 info_message.setForeground(new Color(37, 146, 67));
-                info_message.setText("Kullanici girisi basarili!");
+                info_message.setText("Kullanıcı Girişi Başarılı !");
                 int account_emp_id = rs.getInt("account_emp_id");
                 
-                query = "INSERT INTO employer(emp_id, emp_name, emp_hiring_time, emp_last_login) VALUES (?, ?, ?, ?)";
+                
+            query = "INSERT INTO employer(emp_id, emp_name, emp_hiring_time, emp_last_login) VALUES (?, ?, ?, ?)";
             db.preState=db.con.prepareStatement(query);   
             
             Date date = new Date();    
@@ -187,14 +256,13 @@ public class registerPage extends javax.swing.JFrame {
             db.preState.executeUpdate();
             
             info_message.setBackground(new Color(37,146,67));
-            info_message.setText("Kullanici kaydi basarili!");
+            info_message.setText(" Kullanıcı Girişi Başarılı !");
             
             
             Timer timer=new Timer(1200, null);
             timer.addActionListener(new ActionListener(){
                 @Override
                 public void actionPerformed(ActionEvent arg0) {
-                    
                     
                         setVisible(false);
                         loginPage log=new loginPage();
@@ -213,32 +281,59 @@ public class registerPage extends javax.swing.JFrame {
         } catch (SQLException ex) {
             
             info_message.setForeground(new Color(255, 0, 0));
-            info_message.setText("Kullanici kaydi basarisiz!");
+            info_message.setText("Kullanıcı Girişi Başarısız !");
+            
+            Timer timer=new Timer(1200, null);
+            timer.addActionListener(new ActionListener(){
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    
+                    setVisible(false);
+                    registerPage reg=new registerPage();
+                    reg.registerInject();
+                    timer.stop();
+                }
+                
+            });
+            timer.start();
+            
             
             Logger.getLogger(loginPage.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
     }//GEN-LAST:event_user_registerBTNActionPerformed
+
+    private void user_roleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_user_roleActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_user_roleActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        setVisible(false);
+        loginPage log=new loginPage();
+        log.loginInject();
+    }//GEN-LAST:event_jButton1ActionPerformed
 public void registerInject(){
             java.awt.EventQueue.invokeLater(() -> {
                 new registerPage().setVisible(true);
             });
 }
 
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private java.awt.Button button1;
     private javax.swing.JLabel info_message;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JTextField user_mail;
     private javax.swing.JTextField user_name;
     private javax.swing.JPasswordField user_pass;
     private javax.swing.JPasswordField user_pass2;
     private javax.swing.JButton user_registerBTN;
-    private javax.swing.JTextField user_role;
+    private javax.swing.JComboBox<String> user_role;
     // End of variables declaration//GEN-END:variables
     
 }

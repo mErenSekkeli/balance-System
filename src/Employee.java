6ae1bb33d0931ab1d6c8cb71cb3e23ae1270c072
Selahattin Emre Kutlu <48654612+sel0nidas@users.noreply.github.com>
2022-05-21 18:01:02 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,30 +1,92 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author laptop
- */
 public class Employee {
-    public int id;
+    
+    static int numberOfEmployee=0;
+    
+    public int ID;
     public String name;
-    public int work_time;
-    public int sold_count;
-    public java.util.Date hiring_time;
-    public java.util.Date last_login;
-
-    public Employee(int id, String name, int work_time, int sold_count, java.sql.Date hiring_time, java.sql.Date last_login) {
-        this.id = id;
+    public int workTime;
+    public int soldCount;
+    public Date hiringTime;
+    public Date lastLogin;
+    
+    
+    
+    ArrayList<Employee> employeeInfo = new ArrayList<>();
+    private static Connector db=new Connector();
+    private String query="SELECT * FROM employer";
+    
+    public Employee(int ID,String name, int workTime, int soldCount, Date hiringTime, Date lastLogin) {
+        this.ID = ID;
         this.name = name;
-        this.work_time = work_time;
-        this.sold_count = sold_count;
-        this.hiring_time = hiring_time;
-        this.last_login = last_login;
+        this.workTime = workTime;
+        this.soldCount = soldCount;
+        this.hiringTime = hiringTime;
+        this.lastLogin = lastLogin;
     }
     
     
+    //Boş Constructor
+    public Employee() {
+        
+    }
     
+        public ArrayList<Employee> prepareEmployee() {
+        try {
+            db.preState=db.con.prepareStatement(query);
+         
+            ResultSet rs=db.preState.executeQuery();
+            while(rs.next()){
+                numberOfEmployee++;
+                employeeInfo.add(new Employee(rs.getInt("emp_id"),rs.getString("emp_name"),rs.getInt("emp_work_time"),rs.getInt("emp_sold_count"),rs.getDate("emp_hiring_time"),rs.getDate("emp_last_login")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(analysisCostProfit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return employeeInfo;
+    }
+    
+    //satış olunca veritabanındaki satış sayısını arttırma fonksiyonu
+    public void increaseSoldCountOfEmployee(int employer_id){
+        try {
+            
+            query="UPDATE employer SET emp_sold_count = emp_sold_count + 1 WHERE emp_id = ?";
+            
+            db.preState=db.con.prepareStatement(query);
+            db.preState.setInt(1, employer_id);
+            db.preState.executeUpdate();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Employee.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //iade olunca veritabanındaki satış sayısını azaltma fonksiyonu
+    public void decreaseSoldCountOfEmployee(int employer_id){
+        try {
+            
+            query="UPDATE employer SET emp_sold_count = emp_sold_count - 1 WHERE emp_id = ?";
+            
+            db.preState=db.con.prepareStatement(query);
+            db.preState.setInt(1, employer_id);
+            db.preState.executeUpdate();
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Employee.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        
+    @Override
+    public String toString() {
+        return this.name+", "+this.ID;
+    }
 }

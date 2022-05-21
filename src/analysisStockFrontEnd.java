b71f4baa576dc80876b91c6ae05b76da8d0f824c
Muhammed Eren Şekkeli <100779989+mErenSekkeli@users.noreply.github.com>
2022-05-21 13:30:18 +0300

    Add files via upload

---
+++ src/analysisStockFrontEnd.java
@@ -0,0 +1,145 @@
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * @author MUHAMMEDERENŞEKKELİ
+ */
+public class analysisStockFrontEnd extends javax.swing.JFrame {
+
+    private analysisProductStock anStock;
+    private ProductOperations pr;
+    public analysisStockFrontEnd() {
+        initComponents();
+        anStock=new analysisProductStock();
+        pr=new ProductOperations();
+        getStockAnalysis();
+    }
+    
+    public void inject(){
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new analysisStockFrontEnd().setVisible(true);
+            }
+        });
+    }
+
+    public void getStockAnalysis(){
+        ArrayList<Product> prList=pr.getProductList();
+        ArrayList<String> common=anStock.getAnalysisOfStock();
+        DefaultTableModel table=(DefaultTableModel) stockTable.getModel();
+        int i=0;
+        for(Product p :prList){
+            String[] tmp=new String[4];
+            tmp[0]=Integer.toString(i+1);
+            tmp[1]=p.name;
+            tmp[2]=Integer.toString(p.stock);
+            tmp[3]=common.get(i++);
+            table.addRow(tmp);
+        }
+    }
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        stockTable = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        createExcel = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1280, 720));
+
+        stockTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Ürün No", "Ürün Adı", "Stok Adedi", "Stok Önerisi"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(stockTable);
+        if (stockTable.getColumnModel().getColumnCount() > 0) {
+            stockTable.getColumnModel().getColumn(0).setPreferredWidth(10);
+            stockTable.getColumnModel().getColumn(3).setPreferredWidth(750);
+        }
+
+        jButton1.setText("Kapat");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        createExcel.setText("Excel Oluştur");
+        createExcel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                createExcelActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(createExcel)
+                .addGap(54, 54, 54)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(481, 481, 481))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(70, 70, 70)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(70, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(37, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(createExcel))
+                .addGap(32, 32, 32))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void createExcelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_createExcelActionPerformed
+        try {
+            CSVExporter.jtExportResultSetWithDialog(this, stockTable, true);
+        } catch (SQLException | IOException ex) {
+            Logger.getLogger(ListRefundedProductsUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_createExcelActionPerformed
+
+  
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton createExcel;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable stockTable;
+    // End of variables declaration//GEN-END:variables
+}
